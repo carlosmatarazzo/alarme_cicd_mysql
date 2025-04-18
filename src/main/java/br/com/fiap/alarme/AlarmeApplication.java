2751e8e60d7bf22835c41691f78bd54d25c7c4af
carlosmatarazzo <102484867+carlosmatarazzo@users.noreply.github.com>
2025-04-18 14:37:39 -0300

    teste

--- src/main/java/br/com/fiap/alarme/AlarmeApplication.java
+++ src/main/java/br/com/fiap/alarme/AlarmeApplication.java
@@ -2,8 +2,12 @@ package br.com.fiap.alarme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EntityScan(basePackages = "br.com.fiap.alarme.model")
 public class AlarmeApplication {
 
 	public static void main(String[] args) {
